a60f70766fd13f9d695104731c811f75b8c9238b
helene-cpu <66snahel@stud.akademiet.no>
2025-09-19 15:28:22 +0200

    Enkel side med en funksjon

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return f'''
+<h1>Jeg vet ikke hva jeg skal skrive</h1>
+<ul>
+    <li><a href="/info">Info om meg</a></li>
+    <li><a href="/idk">Jeg vet ikke</a></li>
+    <li><a href="/hva">Hva?</a></li>
+    <li><a href="/navn">Navnet mitt</a></li>
+
+</ul>
+'''
+
+@app.route('/info')
+def info():
+    return 'info : Jeg vet ikke egt'
+
+
+
+if __name__ == "__main__":
+    app.run()
